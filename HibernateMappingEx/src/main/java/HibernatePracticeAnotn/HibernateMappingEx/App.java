3917fe30bc7ac32a63b4118506d3648544069fcf
madhavhub04 <ghatol.madhav@gmail.com>
2023-10-22 13:04:16 +0530

    Hibernate mapping file 1st commit

---
+++ HibernateMappingEx/src/main/java/HibernatePracticeAnotn/HibernateMappingEx/App.java
@@ -0,0 +1,13 @@
+package HibernatePracticeAnotn.HibernateMappingEx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
